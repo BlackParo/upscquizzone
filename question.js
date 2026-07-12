e10c52246d44de9c46ece5aa41833d00c8e2ae0b
BlackParo <pratisyadav@gmail.com>
2026-07-13 03:12:33 +0530

    Update question.js

--- question.js
+++ question.js
@@ -1,6 +1,108 @@
 const questionBank = {
   polity: [
-    // Polity questions
+    // Polity questionspolity: [
+
+{
+question:"India ka Constitution kab lagu hua tha?",
+answers:[
+{text:"26 January 1950",correct:true},
+{text:"15 August 1947",correct:false},
+{text:"26 November 1949",correct:false},
+{text:"2 October 1950",correct:false}
+]
+},
+
+{
+question:"Indian Constitution ka Father kise kaha jata hai?",
+answers:[
+{text:"Dr. B. R. Ambedkar",correct:true},
+{text:"Mahatma Gandhi",correct:false},
+{text:"Jawaharlal Nehru",correct:false},
+{text:"Sardar Patel",correct:false}
+]
+},
+
+{
+question:"Fundamental Rights kis Part me diye gaye hain?",
+answers:[
+{text:"Part III",correct:true},
+{text:"Part II",correct:false},
+{text:"Part IV",correct:false},
+{text:"Part V",correct:false}
+]
+},
+
+{
+question:"Directive Principles kis Part me hain?",
+answers:[
+{text:"Part IV",correct:true},
+{text:"Part III",correct:false},
+{text:"Part I",correct:false},
+{text:"Part VI",correct:false}
+]
+},
+
+{
+question:"Lok Sabha ka maximum tenure kitna hota hai?",
+answers:[
+{text:"5 Years",correct:true},
+{text:"4 Years",correct:false},
+{text:"6 Years",correct:false},
+{text:"7 Years",correct:false}
+]
+},
+
+{
+question:"Rajya Sabha ek...",
+answers:[
+{text:"Permanent House",correct:true},
+{text:"Temporary House",correct:false},
+{text:"Dissolved Every 5 Years",correct:false},
+{text:"State Government",correct:false}
+]
+},
+
+{
+question:"India ke pehle President kaun the?",
+answers:[
+{text:"Dr. Rajendra Prasad",correct:true},
+{text:"S. Radhakrishnan",correct:false},
+{text:"Zakir Hussain",correct:false},
+{text:"V. V. Giri",correct:false}
+]
+},
+
+{
+question:"Article 21 kis se sambandhit hai?",
+answers:[
+{text:"Right to Life",correct:true},
+{text:"Freedom of Speech",correct:false},
+{text:"Equality",correct:false},
+{text:"Education",correct:false}
+]
+},
+
+{
+question:"Election Commission ek...",
+answers:[
+{text:"Constitutional Body",correct:true},
+{text:"Private Body",correct:false},
+{text:"State Body",correct:false},
+{text:"Judicial Body",correct:false}
+]
+},
+
+{
+question:"Fundamental Duties kis Amendment se jode gaye the?",
+answers:[
+{text:"42nd Amendment",correct:true},
+{text:"44th Amendment",correct:false},
+{text:"52nd Amendment",correct:false},
+{text:"61st Amendment",correct:false}
+]
+}
+
+]
   ],
 
   history: [
