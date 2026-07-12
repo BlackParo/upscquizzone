ba6f482713844ff1d9e888cfef4f5347480f614d
BlackParo <pratisyadav@gmail.com>
2026-07-13 02:59:26 +0530

    Create question.js

---
+++ question.js
@@ -0,0 +1,29 @@
+const questionBank = {
+  polity: [
+    // Polity questions
+  ],
+
+  history: [
+    // History questions
+  ],
+
+  geography: [
+    // Geography questions
+  ],
+
+  economy: [
+    // Economy questions
+  ],
+
+  science: [
+    // Science questions
+  ],
+
+  environment: [
+    // Environment questions
+  ],
+
+  current: [
+    // Current Affairs questions
+  ]
+};
